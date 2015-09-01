55e72b69d0fdd00ea8f1891369db63628f0ca3d6
MechaEdgar <ee1206@hotmail.com>
2015-09-01 18:10:42 -0500

    List.py corregido y reverso.py

--- list.py
+++ list.py
@@ -1,6 +1,6 @@
-my_sentences = "im a lumberjack and im ok!"
+my_sentences = "edgar fernando escorza hernandez"
 greeting_list = list("hola amigos estoy en treehouse".split())
 my_list = list(my_sentences.split())
-greeting_list[1] = my_sentences[0]
+greeting_list[4] = my_list[0]
 print(my_list)
 print(greeting_list)
---
+++ reverso.py
@@ -0,0 +1,10 @@
+def stringcases(name):
+     mayus= name.upper()
+     minus= name.lower()
+     titu= name.title()
+     reverse = name[-1:]
+     return mayus
+     return minus
+     return titu
+name= input("give me your name")
+stringcases(name)
